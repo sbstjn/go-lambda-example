e90d8ae085462431c00fa2759fd53f177b6471c2
sbstjn <mail@sbstjn.com>
2018-02-03 17:01:51 +0100

    Update CircleCI config

--- handler/people/people_test.go
+++ handler/people/people_test.go
@@ -18,7 +18,7 @@ func TestWithoutData(t *testing.T) {
 	assert.Equal(t, nil, errRequest)
 	assert.Equal(t, nil, errUnmarshal)
 
-	assert.Equal(t, "{\"data\":[{\"id\":\"d1\",\"name\":\"Anton\",\"age\":31},{\"id\":\"c2\",\"name\":\"Frank\",\"age\":28},{\"id\":\"b1\",\"name\":\"Horst\",\"age\":42}]}", res.Body)
-	assert.Equal(t, 3, len(data.People))
+	assert.Equal(t, "{\"data\":[{\"id\":\"d1\",\"name\":\"Anton\",\"age\":31},{\"id\":\"c2\",\"name\":\"Frank\",\"age\":28},{\"id\":\"b1\",\"name\":\"Horst\",\"age\":42},{\"id\":\"e3\",\"name\":\"Steve\",\"age\":34}]}", res.Body)
+	assert.Equal(t, 4, len(data.People))
 	assert.Equal(t, "Anton", data.People[0].Name)
 }
